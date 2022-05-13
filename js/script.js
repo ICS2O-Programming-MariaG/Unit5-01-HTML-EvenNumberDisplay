0ede8c4d2b2528bc3ddfa1c06947e0a98269d767
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-13 19:46:43 +0000

    Finished calculations for range of numbers to be displayed

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@
 "use strict"
 
 function numbersSent() {
-  //initializing the message variable to an empty string
+  //initializing the numbers variable to an empty string
   let numbers = "";
-  let message = "";
 
   //getting the user values for the minimum and maximum numbers
   let minNumber = parseInt(document.getElementById("min-number").value);
@@ -18,12 +17,16 @@ function numbersSent() {
   //initializing the counter variable to the minNumber
   let counter = minNumber;
   
-  //number % 2 == 0 gets makes sure remainder is 0, indicating that number is divisible by 2 (even number)
+  //while loop that specifies the range, so the code within the loop executes as long as the counter is greater than or equal to the minimum and less than or equal to the maximum
   while ((counter >= minNumber) && (counter <= maxNumber)) {
-    counter = counter + 1;
+    //if statement checks for even numbers, for as long as the counter is still less than or equal to the maximum
+    //number % 2 == 0 makes sure remainder is 0, indicating that number is divisible by 2 (even number)
     if ((counter <= maxNumber) && (counter % 2 == 0)) {
       numbers = numbers + counter + "<br>";
     }
+    counter = counter + 1;
   }
-  document.getElementById("results").innerHTML = numbers;
+
+  //displaying the results (the range of even numbers) back to the user
+  document.getElementById("results").innerHTML = "Here is a list of all the even numbers between the minimum and maximum that you entered:<br><br>" + numbers;
 }
