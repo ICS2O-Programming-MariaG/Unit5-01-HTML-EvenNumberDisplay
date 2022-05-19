bbc2f8db3e7abca6acb1bc4750d1dcf999048694
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-19 00:05:34 +0000

    Added error message for when min is greater than max

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@
 "use strict"
 
 function numbersSent() {
-  //initializing the numbers variable to an empty string
+  //initializing the numbers and message variable to an empty string
   let numbers = "";
+  let message = "";
 
   //getting the user values for the minimum and maximum numbers
   let minNumber = parseInt(document.getElementById("min-number").value);
@@ -16,17 +17,25 @@ function numbersSent() {
 
   //initializing the counter variable to the minNumber
   let counter = minNumber;
-  
-  //while loop that specifies the range, so the code within the loop executes as long as the counter is greater than or equal to the minimum and less than or equal to the maximum
-  while ((counter >= minNumber) && (counter <= maxNumber)) {
-    //if statement checks for even numbers, for as long as the counter is still less than or equal to the maximum
-    //number % 2 == 0 makes sure remainder is 0, indicating that number is divisible by 2 (even number)
-    if ((counter <= maxNumber) && (counter % 2 == 0)) {
-      numbers = numbers + counter + "<br>";
+
+  //if statement checks if minNumber > maxNumber (there is an error)
+  if (minNumber > maxNumber) {
+    message = "Please make sure the minimum is less than or equal to the maximum.";
+  }
+  //else, complete while loop to create list of even numbers between min and max
+  else {
+    //while loop that specifies the range, so the code within the loop executes as long as the counter is greater than or equal to the minimum and less than or equal to the maximum
+    while ((counter >= minNumber) && (counter <= maxNumber)) {
+      //if statement checks for even numbers, for as long as the counter is still less than or equal to the maximum
+      //number % 2 == 0 makes sure remainder is 0, indicating that number is divisible by 2 (even number)
+      if (counter % 2 == 0) {
+        numbers = numbers + counter + "<br>";
+      }
+      counter = counter + 1;
+      message = "Here is a list of all the even numbers between the minimum and maximum that you entered:<br><br>" + numbers;
     }
-    counter = counter + 1;
   }
 
   //displaying the results (the range of even numbers) back to the user
-  document.getElementById("results").innerHTML = "Here is a list of all the even numbers between the minimum and maximum that you entered:<br><br>" + numbers;
+  document.getElementById("results").innerHTML = message;
 }
